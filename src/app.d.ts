ad344725c0b762dccb504dc9a0070a06198e58cd
leartpro <lenni.protte@gmx.de>
2023-08-15 20:32:42 +0200

    introduce scss/sass and carbon-design system

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,7 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import 'carbon-components/scss/globals/scss/styles.scss';
+
 declare global {
 	namespace App {
 		// interface Error {}
