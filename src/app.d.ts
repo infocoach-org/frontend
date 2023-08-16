be23454ea9a6da3c36aeecad8c474993a67e302c
leartpro <lenni.protte@gmx.de>
2023-08-16 16:10:40 +0200

    applying carbon css

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,5 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-import 'carbon-components/scss/globals/scss/styles.scss';
 
 declare global {
 	namespace App {
